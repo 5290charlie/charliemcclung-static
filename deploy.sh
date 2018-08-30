13d5671b0a96211eec5aa827c24abe7dcd7396c9
Charlie McClung <charlie@cmr1.com>
2018-08-30 13:42:28 -0600

    Housekeeping on resume & personal site

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws s3 cp public/ s3://charliemcclung.com/ --recursive
+aws s3 sync public/ s3://charliemcclung.com/ --delete
+aws cloudfront create-invalidation --distribution-id EWQ3XGFZ5HLNL --paths "/*"
